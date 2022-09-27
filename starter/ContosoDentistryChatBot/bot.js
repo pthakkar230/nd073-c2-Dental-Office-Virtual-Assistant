e85e3968418b370278080b312ea8ff5a0fe30d66
Parth Thakkar <parththakkar@Parths-MacBook-Pro.local>
2022-09-27 15:22:26 -0400

    u

--- starter/ContosoDentistryChatBot/bot.js
+++ starter/ContosoDentistryChatBot/bot.js
@@ -7,6 +7,8 @@ const { QnAMaker } = require('botbuilder-ai');
 const DentistScheduler = require('./dentistscheduler');
 const IntentRecognizer = require("./intentrecognizer")
 
+const BOT_FUNCTION_STRING = "I can help you get the availability at the dental office, schedule appointments, and answer any questions about our practice."
+
 class DentaBot extends ActivityHandler {
     constructor(configuration, qnaOptions) {
         // call the parent constructor
@@ -14,12 +16,13 @@ class DentaBot extends ActivityHandler {
         if (!configuration) throw new Error('[QnaMakerBot]: Missing parameter. configuration is required');
 
         // create a QnAMaker connector
-        this.QnAMaker = new QnAMaker(configuration)
+        this.QnAMaker = new QnAMaker(configuration.QnAConfiguration, qnaOptions)
        
         // create a DentistScheduler connector
-        this.DentistScheduler = new DentistScheduler(configuration)
+        this.DentistScheduler = new DentistScheduler(configuration.SchedulerConfiguration)
       
         // create a IntentRecognizer connector
+        this.IntentRecognizer = new IntentRecognizer(configuration.LuisConfiguration)
 
 
         this.onMessage(async (context, next) => {
@@ -38,6 +41,29 @@ class DentaBot extends ActivityHandler {
             //  return;
             // }
             // else {...}
+
+            const qnaResults = await this.QnAMaker.getAnswers(context);
+
+            const LuisResult = await this.IntentRecognizer.executeLuisQuery(context);
+
+            if (LuisResult.luisResult.prediction.topIntent === "ScheduleAppointment" &&
+            LuisResult.intents.ScheduleAppointment.score > .6 &&
+            LuisResult.entities.$instance &&
+            LuisResult.entities.$instance.datetime &&
+            LuisResult.entities.$instance.datetime[0]) {
+                const datetime = LuisResult.entities.$instance.datetime[0].text;
+                const response = await this.DentistScheduler.scheduleAppointment(datetime);
+                await context.sendActivity(response);
+            } else if (LuisResult.luisResult.prediction.topIntent === "GetAvailability" && LuisResult.intents.GetAvailability.score > .6) {
+                const response = await this.DentistScheduler.getAvailability();
+                await context.sendActivity(response);
+            } else if (qnaResults[0]) {
+                await context.sendActivity(`${qnaResults[0].answer}`)
+            } else {
+                await context.sendActivity("I'm not sure I can answer your question." + BOT_FUNCTION_STRING)
+            }
+
+
              
             await next();
     });
@@ -45,7 +71,7 @@ class DentaBot extends ActivityHandler {
         this.onMembersAdded(async (context, next) => {
         const membersAdded = context.activity.membersAdded;
         //write a custom greeting
-        const welcomeText = 'Hello!!!';
+        const welcomeText = 'Welcome! ' + BOT_FUNCTION_STRING;
         for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
             if (membersAdded[cnt].id !== context.activity.recipient.id) {
                 await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
